4d80f23811c34da44c1400e06af355a24c5e136a
bimlas <bimbalaszlo@gmail.com>
2020-06-20 18:01:03 +0200

    Minimal sprite test
    * Clear the whole screen
    * Blit the sprite to screen
    * Flip the display
    
    450 FPS

---
+++ playground/fps_optimization/01_basic_drawing.py
@@ -0,0 +1,22 @@
+import playground
+import pygame
+
+
+sprite = pygame.sprite.Sprite()
+sprite.image = pygame.Surface((100, 100))
+sprite.image.fill((128, 128, 128))
+sprite.rect = sprite.image.get_rect()
+
+
+def update(screen: pygame.Surface):
+    screen.fill((0, 0, 0))
+    screen.blit(sprite.image, sprite.rect)
+    pygame.display.flip()
+
+
+def event_handler(event: pygame.event.Event):
+    if event.type == pygame.MOUSEMOTION:
+        sprite.rect.move_ip(event.rel)
+
+
+playground.loop(update, event_handler)
