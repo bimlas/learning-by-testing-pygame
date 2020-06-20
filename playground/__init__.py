0a62eecce3ce8a78404cc2978de1c98c6a627b74
bimlas <bimbalaszlo@gmail.com>
2020-06-20 18:00:02 +0200

    Add a skeleton for custom Pygame playgrounds

---
+++ playground/__init__.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+def loop(update, event_handler):
+    screen_width = 800
+    screen_height = 600
+    max_fps = 9999
+    should_print_fps = True
+
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+
+    # Game loop
+    running = True
+    while running:
+        clock.tick(max_fps)
+        if should_print_fps:
+            print('FPS: {}'.format(clock.get_fps()))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            event_handler(event)
+
+        update(screen)
+        # Call pygame.display.flip() from the callback!
+
+    pygame.quit()
